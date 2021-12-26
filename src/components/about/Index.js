38b3238aafd123112882826303793b152dd69727
brraut <bishowraut@gmail.com>
2021-12-26 18:35:14 +0545

    add

--- src/components/about/Index.js
+++ src/components/about/Index.js
@@ -13,7 +13,7 @@ function AboutIndex() {
         <div className="about-title">About Me</div>
         <div className="about-wrapper">
           <Row>
-            <Col md={8} order>
+            <Col md={{ span: 8, order: 1 }} xs={{ span: 12, order: 2 }} order>
               <div className="about-content">
                 <p>
                   Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sed
@@ -32,7 +32,7 @@ function AboutIndex() {
                 </p>
               </div>
             </Col>
-            <Col md={4}>
+            <Col md={{ span: 4, order: 2 }} xs={{ span: 12, order: 1 }}>
               <div className="profile-wrapper">
                 <img src="/images/profile.jpeg" alt="" />
               </div>
